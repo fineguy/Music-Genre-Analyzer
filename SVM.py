46c56f57d7c948520078a0fec560695ef9b1f1bc
Tim <timasem@gmail.com>
2015-04-11 02:01:05 +0800

    Support Vector Machine classifier

--- SVM.py
+++ SVM.py
@@ -5,42 +5,36 @@ Created on Mon Apr  6 14:12:31 2015
 @author: timasemenov
 """
 
-import numpy as np
+
 import scipy.io as sio
 from sklearn import cross_validation
 from sklearn import svm
 
-data = sio.loadmat('data.mat')
-contents_X = data['X']
+# loads data
+data = sio.loadmat('data2.mat')
+X = data['X']
 contents_y = data['y']
 
 y = []
-X = []
 
+# convert y from cell array to int array
 for i in range(len(contents_y[0])):
-    a = contents_X[0, i]
-    b = []
-    
-    for j in range(len(a)):
-        for k in range(2):
-            b.append(a[j][k])
-    if (len(b) == 800):
-        X.append(b)
-        y.append(contents_y[0][i])
+    y.append(contents_y[0][i])
 
+# divide data into train and test sets
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=1)
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.6, random_state=1)
+# create a Support Vector Machine classifier
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+count = 0       # counter for correctly predicted values
 
-lin_clf = svm.LinearSVC()
-lin_clf.fit(X_train, y_train)
-count = 0
 for i in range(len(y_test)):
-    a = lin_clf.predict(X_test[i])
+    a = clf.predict(X_test[i])
     b = y_test[i]
-    print("Predicted value: " + str(a) + ", actual value: " + str(b))
+    print("Predicted value: " + str(a[0]) + ", actual value: " + str(b))
     if (a == b):
         count += 1
 
-print len(y_train)
-print "Correctly predicted: " + str(count) + ", total examples: " + str(len(y_test))
-    
+print str(len(y_train)) + " examples used to train the model"
+print "Correctly predicted : " + str(count) + ", total examples: " + str(len(y_test)) + ", percentage: " + str(count*100.0/len(y_test))
