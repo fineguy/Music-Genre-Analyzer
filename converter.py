541de652f8f5c8bfc96ee02a6c48c29610ee4822
Tim <timasem@gmail.com>
2015-04-22 18:40:05 +0800

    Converter

--- converter.py
+++ converter.py
@@ -17,27 +17,33 @@ pydub.AudioSegment.converter = '/usr/local/Cellar/ffmpeg/2.6.1/bin/ffmpeg'
 genres = ['Rap', 'Rock']        # List of different genres
 music = '/Users/timasemenov/ML' # path to the music folder
 size = 0                        # number of training examples stored
-
-
-def sample(mp3, time, rate):
+X = []                          # training set
+y = []                          # label set
+    
+def sample(mp3, time, rate, index):
+    global X, y    
+    
     duration = len(mp3)
-    snippet = mp3[(duration-time*1000)/2 : (duration+time*1000)/2]
-    temp = path.join('/Users/timasemenov', 'temp.wav')
-    snippet.export(temp, format='wav')      # convert an mp3 file to wav format
-    fs, data = wavfile.read(temp)           # read the .wav file as numpy array
-    step = 44100 / rate
-    remove(temp)                            # delete the temporary .wav file
-    return data[::step]
-
+    parts = duration / 1000 / time
+    temp = path.join('/Users/timasemenov', 'temp.wav')    
+    mp3.export(temp, format='wav')      # convert an mp3 file to wav format    
+    fs, data = wavfile.read(temp)       # read the .wav file as numpy array
+    remove(temp)                        # delete the temporary .wav file
+    for array in np.split(data[:parts*time*44100], parts):
+        x = np.reshape(np.cumsum(np.split(np.array(array), rate), 1)[:,-1], 2*rate)
+        X.append(x)
+        y.append(index)
+        
+        
 def save(X, y):
-    sio.savemat('data.mat', {'X':X, 'y':y})
+    sio.savemat('data.mat', {'X':np.array(X), 'y':np.array(y)})
+
 
 # time = duration in seconds of the audio samples we analyze
 # rate = audio sampling rate in hertz
 def main(time, rate):
-    global size, genres, music
-    X = []    # training set
-    y = []                             # label set
+    global size, genres, music, X, y
+
     for genre in genres:
         path_to_genre = path.join(music, genre)
         for song in listdir(path_to_genre):
@@ -45,12 +51,11 @@ def main(time, rate):
             try:
                 music_mp3 = pydub.AudioSegment.from_mp3(path_to_song) 
                 # add current .mp3 file to the training set
-                X.append(sample(music_mp3, time, rate))
-                y.append(genres.index(genre))
-                size += 1
-                print(size)
+                sample(music_mp3, time, rate, genres.index(genre))
+                print(song)
             except pydub.exceptions.CouldntDecodeError:
                 print "Couldn't decode " + song
     save(X, y) 
-        
+  
+      
 main(20, 20)
